0e6abbdcfadeccfe4b3914ee2c2cc69c287d9fe2
Sudipta Ghoshal <ghoshalsudipta36@gmail.com>
2025-01-05 21:49:57 +0530

    test terminal

--- to-do-app/vite-project/src/App.jsx
+++ to-do-app/vite-project/src/App.jsx
@@ -7,7 +7,7 @@ import TodoList from './components/Todo'
 function ToDoApp() {
   return (
     <div className='flex min-h-screen justify-center items-center bg-primary py-10'>
-      <div className='w-[432px]'>
+      <div className='w-[432px] myClass'>
         <AddTask />
         <TodoList />
         <DoneList />
